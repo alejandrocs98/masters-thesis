3d791f6000ed39ca974857457f79de29c11c20f2
Alejandro Castellanos <a.castellanoss@uniandes.edu.co>
2023-03-01 20:22:46 -0500

    shrink-factor calculation and others

--- ecological-modeling/mdsine2_inference_pipeline.sh
+++ ecological-modeling/mdsine2_inference_pipeline.sh
@@ -5,6 +5,6 @@ conda activate mdsine2
 
 for dataset in LF0 HF0; do
 	for seed in 0 4 27; do
-		python mdsine2_inference_pipeline.py $dataset $seed > mcnulty-results/$dataset/$(echo $dataset)_seed$seed.log 2>&1;
+		python mdsine2_inference_pipeline.py $dataset $seed > $(echo $dataset)_seed$seed.log 2>&1;
 	done;
 done;
